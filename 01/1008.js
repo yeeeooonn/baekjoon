89d56fdf0c6e6ab5ad506b396a34fb0975678822
yeeeooonn <73294363+yeeeooonn@users.noreply.github.com>
2022-03-30 17:45:45 +0900

    baekjoon_1008

---
+++ 01/1008.js
@@ -0,0 +1,11 @@
+let fs = require('fs');
+//백준
+//let input = fs.readFileSync('/dev/stdin').toString().split(' ');
+
+//VSCode
+let input = fs.readFileSync('예제.txt').toString().split(' ');
+
+let a = parseInt(input[0]);
+let b = parseInt(input[1]);
+
+console.log(a / b);
