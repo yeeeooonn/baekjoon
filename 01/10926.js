4c7a0c80a214f9ae36128bc8c9e1a8c078a6a114
yeeeooonn <73294363+yeeeooonn@users.noreply.github.com>
2022-04-04 16:38:30 +0900

    baekjoon_10926

---
+++ 01/10926.js
@@ -0,0 +1,9 @@
+let fs = require('fs');
+
+//백준
+//let input = fs.readFileSync('/dev/stdin').toString().trim();
+
+//VSCode
+let input = fs.readFileSync('예제.txt').toString().trim();
+
+console.log(input + '??!');
