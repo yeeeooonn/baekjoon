418e1ff8bbf806734560ed1a5c2df3d0c4dc9c34
yeeeooonn <73294363+yeeeooonn@users.noreply.github.com>
2022-09-15 14:55:23 +0900

    baekjoon 1316

---
+++ 06/1316.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const { createBrotliCompress } = require('zlib');
+
+//백준
+const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+//VSCode
+//const input = fs.readFileSync('예제.txt').toString().split('\n');
+
+let count = 0;
+
+for (let i = 1; i <= input[0]; i++) {
+	let check = 1; //그룹단어인지 체크
+
+	let word = input[i];
+	let prevWord = word[0];
+
+	for (let j = 1; j < input[i].length; j++) {
+		if (word[j] != prevWord && word.slice(j).includes(prevWord)) {
+			check = 0;
+			break;
+		} else {
+			prevWord = word[j];
+		}
+	}
+	if (check == 1) count++;
+}
+
+console.log(count);
